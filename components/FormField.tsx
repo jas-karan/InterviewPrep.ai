bc082f036e4b461546f16521cde1b7864930c59d
Jaskaran <262jaskaran@gmail.com>
2025-10-19 17:58:38 +0530

    fix

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -19,7 +19,7 @@ interface FormFieldProps<T extends FieldValues>{
     type?: 'text' | 'password' | 'email';
 }
 
-const FormField = ({ control, name, label, placeholder, type="text" } : FormFieldProps<T>) => (
+const FormField = ({ control, name, label, placeholder, type="text" } : FormFieldProps<FieldValues>) => (
     <Controller
         name={name}
         control={control}
